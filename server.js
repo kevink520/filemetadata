6d79112ba6223e07fbf4df14a07a04bb7c8e6767
Kevin Kim <kevinkim75@gmail.com>
2018-09-25 20:41:02 -0400

    Completed the app.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer = require('multer');
+var upload = multer();
 
 var app = express();
 
@@ -11,11 +13,27 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  var upfile = req.file;
+  if (!upfile) {
+    return res.json({ error: 'File is missing' });
+  }
+
+  var fileName = upfile.originalname;
+  var mimeType = upfile.mimetype;
+  var fileSize = upfile.size;
+  res.json({
+    name: fileName,
+    type: mimeType,
+    size: fileSize,
+  });
 });
 
 app.listen(process.env.PORT || 3000, function () {
